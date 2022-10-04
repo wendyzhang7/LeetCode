cdb2d9d6d95868b00db9f17cc3e9063ae7fec4c0
Wendy Zhang <wzhang5252@gmail.com>
2022-10-04 13:44:01 -0400

    Time: 172 ms (5.25%), Space: 46.7 MB (56.71%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var removeElements = function(head, val) {
+
+    let dummy = new ListNode(-101);
+    dummy.next = head;
+    let current = dummy;
+    
+    while (current && current.next) {
+        if (current.next.val === val) {
+            current.next = current.next.next;
+        } 
+        else {
+            current = current.next;
+        };
+    }
+    
+    return dummy.next;
+};
