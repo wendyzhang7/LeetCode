17dce95888b63ce38928dbbe85f559ef03899c7b
Wendy Zhang <wzhang5252@gmail.com>
2022-10-29 14:27:15 -0400

    Time: 100 ms (46.94%), Space: 41.8 MB (83.93%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.js
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.js
@@ -3,5 +3,9 @@
  * @return {number}
  */
 var minimumOperations = function(nums) {
-    return new Set(nums.filter(Boolean)).size;
+    //hint 3: the answer is the number of unique non-zero numbers in input
+    //so first filter out values that is not 0
+    //then get the unique number of input by using Set
+    //then Set.prototype.size gives number of elements in the array
+    return new Set(nums.filter(val => val!==0)).size;
 };
