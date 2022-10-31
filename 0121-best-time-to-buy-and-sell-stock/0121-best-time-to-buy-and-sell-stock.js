7f6695e9ef7f2ee65142f41726e38c53392af2a5
Wendy Zhang <wzhang5252@gmail.com>
2022-10-31 12:30:33 -0400

    Time: 141 ms (55.09%), Space: 51.8 MB (52.01%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    var lowest = prices[0];
+    var maxProfit = 0;
+        
+    for (var i=0; i<prices.length; i++) {
+        if (prices[i] < lowest) {
+            lowest = prices[i];
+        } else {
+            maxProfit = Math.max(maxProfit, prices[i] - lowest)
+        }
+    }
+    
+    return maxProfit;
+};
