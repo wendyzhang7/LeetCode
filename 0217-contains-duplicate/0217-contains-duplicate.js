5453df0c36214fa55bb4471462c7d6771a477e48
Wendy Zhang <wzhang5252@gmail.com>
2022-11-21 11:36:56 -0500

    Time: 354 ms (19.40%), Space: 50.4 MB (75.72%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -4,16 +4,14 @@
  */
 var containsDuplicate = function(nums) {
     nums=nums.sort((a,b)=>(a-b));
-    // console.log(nums);
-    // if (nums.length <= 1) return false;
     
-    for (var i=0; i<nums.length; i++) {
-        var current = nums[i];
-        for (var k=i+1; k<nums.length; k++) {
-            if (nums[i] === nums[k]) {
-                return true;
-            } 
-        }
+    for (var i=0; i<nums.length -1; i++) {
+        if (nums[i] === nums[i+1]) return true;
+        // for (var k=i+1; k<nums.length; k++) {
+        //     if (nums[i] === nums[k]) {
+        //         return true;
+        //     } 
+        // }
     }
     return false;
 };
